f563bdc920889cb5d84aec4b3b007fbd8a756d1d
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2025-09-08 13:00:34 +0100

    Added basic app.js with single image device display

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import ElectricKettle from './assets/images/electricKettle.jpg'
 
 
 export default function App() {
@@ -8,7 +9,14 @@ export default function App() {
 
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Text>NEW PROJECT (Smart Home App) 🌝</Text>
+      <Text>SMART HOME APP</Text>
+      <View style={styles.deviceCard}>
+        <Image
+          source={ElectricKettle}
+          style={styles.deviceImage}
+        />
+        <Text style={styles.deviceName}>Electric Kettle</Text>
+      </View>
     </View>
   );
 }
@@ -16,8 +24,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#1a1a1a',
+    padding: 20,
+    paddingTop: 60, // for status bar
   },
+  title: {
+    fontSize: 24,
+    color: '#FFFFFF',
+    // marginBottom: 
+  },
+  deviceCard: {
+    width: 60,
+    height: 60,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignItems: 'center',
+    // backgroundColor: '#5b0707ff'
+  },
+  deviceImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 8,
+    // marginBottom:
+  },
+  deviceName: {
+    fontSize: 16,
+    color: '#FFFFFF',
+  }
 });
