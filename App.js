46d1e53ca43bb1e77cf6732d164c9ad17a4bbea5
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2025-09-08 19:46:15 +0100

    improved device card UI

--- App.js
+++ App.js
@@ -8,14 +8,31 @@ export default function App() {
   return (
 
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>SMART HOME APP</Text>
+      <Text style={styles.title}>SMART HOME APP</Text>
+
+
       <View style={styles.deviceCard}>
-        <Image
+
+        {/*  top section */}
+        <View style={styles.topSection}>
+          {/* left */}
+          <View style={styles.deviceInfo}>
+            <Text style={styles.deviceName}>Electric Kettle</Text>
+            <Text style={styles.deviceLocation}>Kitchen</Text>
+          </View>
+          {/* right */}
+          <View style={styles.toggleButton}>
+            <Text style={styles.placeholderText}>Toggle</Text>
+          </View>
+        </View>
+
+        {/* Bottom */}
+        <View style={styles.bottomSection}>
+          <Image
           source={ElectricKettle}
           style={styles.deviceImage}
-        />
-        <Text style={styles.deviceName}>Electric Kettle</Text>
+          />
+        </View>
       </View>
     </View>
   );
@@ -31,24 +48,62 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     color: '#FFFFFF',
-    // marginBottom: 
+    marginBottom: 30,
   },
+  
   deviceCard: {
-    width: 60,
-    height: 60,
+    width: 180,
+    height: 170,
+    backgroundColor: '#2d2d2d',
     borderRadius: 8,
     marginBottom: 10,
-    alignItems: 'center',
-    // backgroundColor: '#5b0707ff'
+    padding: 10,
+    // alignItems: 'center',
   },
+  topSection: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flex: 1,
+    marginBottom: 10,
+  },
+  deviceInfo: {
+    // flex: 1
+  },
+  deviceName: {
+    display: 'flex',
+    fontSize: 16,
+    color: '#ffffff',
+    fontWeight: 'bold',
+    marginBottom: 4,
+  },
+  deviceLocation: {
+    fontSize: 14,
+    color: '#b0b0b0',
+  },
+
+  // right side of top
+  toggleButton: {
+    backgroundColor: '#444',
+    padding: 8,
+    fontSize: 12,
+  },
+
+  placeholderText: {
+    color: '#FFFFFF',
+    fontSize: 12,
+  },
+  
+  // buttomSection:
+  bottomSection: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
   deviceImage: {
     width: 60,
     height: 60,
     borderRadius: 8,
     // marginBottom:
   },
-  deviceName: {
-    fontSize: 16,
-    color: '#FFFFFF',
-  }
 });
