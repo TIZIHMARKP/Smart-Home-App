50d085be3fb885bfc095e5e306ec1749283e8af4
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2025-09-17 23:17:55 +0100

    Created basic voice screen UI

--- screens/Voice/VoiceScreen.js
+++ screens/Voice/VoiceScreen.js
@@ -4,9 +4,27 @@ import { View, Text, StyleSheet } from 'react-native';
 function VoiceScreen() {
   return (
     <View style={styles.container}>
+       
+      <View style={styles.topSection}>
+        <Text style={styles.statusText}>• Listening...</Text>
+      </View>
+      
+       
+      <View style={styles.middleSection}>
+        {/* <View style={styles.middleText}>
+          <Text>Turn on</Text>
+          <Text>Air conditioner</Text>
+        </View> */}
 
-      <Text style={styles.text}> Voice Screen </Text>
-
+        <View style={styles.voiceFace}>
+          <Text style={styles.faceText}>😊</Text>
+        </View>
+      </View>
+      
+       
+      <View style={styles.bottomSection}>
+        <Text style={styles.instructionText}>Tap to speak</Text>
+      </View>
     </View>
   );
 }
@@ -17,11 +35,49 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#1a1a1a',
+  },
+  topSection: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 50,
+  },
+  statusText: {
+    // color: '#4ECDC4',
+    color: '#ffffff',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  middleSection: {
+    flex: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: 'red'
+  },
+  // middleText: {
+  //   margin: 10,
+  //   color: "#fff"
+  // },
+  voiceFace: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    backgroundColor: '#2d2d2d',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    color: '#ffffff',
-    fontSize: 18,
+  faceText: {
+    marginTop: -25,
+    fontSize: 250,
+  },
+  bottomSection: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 100,
+  },
+  instructionText: {
+    color: '#B0B0B0',
+    fontSize: 14,
   },
 });
